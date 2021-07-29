1d384fe508eb11b0b0c5eb5e9004f56b0408b0de
Cjain0 <chinmayjain99@gmail.com>
2021-07-29 00:00:16 -0600

    Graphing going decent, Almost Done

--- analysis/Chinmay Jain/Scripts/project_functions.py
+++ analysis/Chinmay Jain/Scripts/project_functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 from multipledispatch import dispatch 
 
 def load_and_process(path):
@@ -10,6 +11,8 @@ def load_and_process(path):
         pd.DataFrame(
             pd.read_csv(path)
         )
+        .dropna(axis=0)
+        #.shape()
     )
     # Method Chain 2 (Create new columns, drop others, and do processing)
 
@@ -63,3 +66,34 @@ def data_filter(league,path):
     av = {'League':league, 'pts': df2['pts'].mean(), 'xG':df2['xG'].mean(), 'xGA':df2['xGA'].mean(), 'ppda_coef':df2['ppda_coef'].mean(), 'oppda_coef':df2['oppda_coef'].mean(), 'deep':df2['deep'].mean(), 'deep_allowed':df2['deep_allowed'].mean()}
     
     return av
+
+def weighted_av(path):
+    df_av = pd.DataFrame(data_filter("La_liga", path), index=[0])
+    df_av = df_av.append(data_filter("EPL", path), ignore_index=True)
+    df_av = df_av.append(data_filter("Bundesliga", path), ignore_index=True)
+    df_av = df_av.append(data_filter("Serie_A", path), ignore_index=True)
+    df_av = df_av.append(data_filter("Ligue_1", path), ignore_index=True)
+    
+    df_av['Offensive W_Average'] = 0.5*df_av['xG'] + 0.25*df_av['ppda_coef'] + 0.25*df_av['deep']
+    df_av['Defensive W_Average'] = 0.45*df_av['xGA'] + 0.35*df_av['oppda_coef'] + 0.2*df_av['deep_allowed']
+    df_av = df_av.drop(columns = ['pts', 'xG', 'xGA', 'ppda_coef', 'oppda_coef', 'deep', 'deep_allowed'])
+    
+    df_top = pd.DataFrame(data_filter("La_liga",1, path), index=[0])
+    df_top = df_top.append(data_filter("EPL",1, path), ignore_index=True)
+    df_top = df_top.append(data_filter("Bundesliga",1, path), ignore_index=True)
+    df_top = df_top.append(data_filter("Serie_A",1, path), ignore_index=True)
+    df_top = df_top.append(data_filter("Ligue_1",1, path), ignore_index=True)
+    
+    df_top['Offensive W_Average'] = 0.5*df_top['xG'] + 0.25*df_top['ppda_coef'] + 0.25*df_top['deep']
+    df_top['Defensive W_Average'] = 0.45*df_top['xGA'] + 0.35*df_top['oppda_coef'] + 0.2*df_top['deep_allowed']
+    df_top = df_top.drop(columns = ['pts', 'xG', 'xGA', 'ppda_coef', 'oppda_coef', 'deep', 'deep_allowed'])
+    #df_top = df_top.rename(columns={'League':'Top Team'})
+    df=pd.concat([df_top,df_av])
+    df = df.sort_values(by=['League'], ignore_index=True)
+    #df = pd.merge(df_av, df_top, how = 'outer')
+    return df
+    
+    
+
+
+    
