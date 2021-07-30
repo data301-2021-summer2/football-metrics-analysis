6b50f81315f723432e8d8ce1ac06e392cead5843
lakshaykarnwal <lakshaykarnwal@gmail.com>
2021-07-30 02:13:32 -0700

    Added graphs for EDA

--- analysis/Lakshay Karnwal/Scripts/project_functions.py
+++ analysis/Lakshay Karnwal/Scripts/project_functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from multipledispatch import dispatch
+import matplotlib.pyplot as plt
 
 def printing():
     print("Hello world")
@@ -23,9 +24,9 @@ def load_and_process(path):
           df1
     .assign(pts_per_game = lambda df: df['pts']/df['matches']
     )
-    .drop(columns=['xGA_diff','npxG','npxGD','ppda_coef','deep','deep_allowed','npxGA','oppda_coef','wins','draws','loses']
+    .drop(columns=['xGA_diff','npxG','npxGD','deep','deep_allowed','npxGA','wins','draws','loses']
     )
-    .rename(columns={'Unnamed: 0' :'league','Unnamed: 1':'year','missed':'conceded'}
+    .rename(columns={'Unnamed: 0' :'league','Unnamed: 1':'year','missed':'conceded','ppda_coef':'pressure','oppda_coef':'oppo_pressure'}
     )
     .set_index('league'
     )
@@ -59,6 +60,72 @@ def filter_data_set(league, year):
     #print(df2['xG'])
     
     return df4
+
+def bar_plot(ax, data, colors=None, total_width=0.8, single_width=1, legend=True):
+    """Draws a bar plot with multiple bars per data point.
+
+    Parameters
+    ----------
+    ax : matplotlib.pyplot.axis
+        The axis we want to draw our plot on.
+
+    data: dictionary
+        A dictionary containing the data we want to plot. Keys are the names of the
+        data, the items is a list of the values.
+
+        Example:
+        data = {
+            "x":[1,2,3],
+            "y":[1,2,3],
+            "z":[1,2,3],
+        }
+
+    colors : array-like, optional
+        A list of colors which are used for the bars. If None, the colors
+        will be the standard matplotlib color cyle. (default: None)
+
+    total_width : float, optional, default: 0.8
+        The width of a bar group. 0.8 means that 80% of the x-axis is covered
+        by bars and 20% will be spaces between the bars.
+
+    single_width: float, optional, default: 1
+        The relative width of a single bar within a group. 1 means the bars
+        will touch eachother within a group, values less than 1 will make
+        these bars thinner.
+
+    legend: bool, optional, default: True
+        If this is set to true, a legend will be added to the axis.
+    """
+
+    # Check if colors where provided, otherwhise use the default color cycle
+    if colors is None:
+        colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+    # Number of bars per group
+    n_bars = len(data)
+
+    # The width of a single bar
+    bar_width = total_width / n_bars
+
+    # List containing handles for the drawn bars, used for the legend
+    bars = []
+
+    # Iterate over all data
+    for i, (name, values) in enumerate(data.items()):
+        # The offset in x direction of that bar
+        x_offset = (i - n_bars / 2) * bar_width + bar_width / 2
+
+        # Draw a bar for every value of that type
+        for x, y in enumerate(values):
+            bar = ax.bar(x + x_offset, y, width=bar_width * single_width, color=colors[i % len(colors)])
+
+        # Add a handle to the last drawn bar, which we'll need for the legend
+        bars.append(bar[0])
+
+    # Draw legend if we need
+    if legend:
+        ax.legend(bars, data.keys())
+
 
 @dispatch(str,int)
 def data_filter(league,year):
