850f56fd693a62ad7a47963d01a686d86d569cd5
lakshaykarnwal <lakshaykarnwal@gmail.com>
2021-07-30 00:35:31 -0700

    Added a new function

--- analysis/Lakshay Karnwal/Scripts/project_functions.py
+++ analysis/Lakshay Karnwal/Scripts/project_functions.py
@@ -14,7 +14,7 @@ def load_and_process(path):
         .dropna(axis=0 #new line added
         )
         #.shape(
-        )
+        
     )
 
     # Method Chain 2 (Create new columns, drop others, and do processing)
@@ -25,7 +25,7 @@ def load_and_process(path):
     )
     .drop(columns=['xGA_diff','npxG','npxGD','ppda_coef','deep','deep_allowed','npxGA','oppda_coef','wins','draws','loses']
     )
-    .rename(columns={'Unnamed: 0' :'league','Unnamed: 1':'year','missed:conceded'}
+    .rename(columns={'Unnamed: 0' :'league','Unnamed: 1':'year','missed':'conceded'}
     )
     .set_index('league'
     )
@@ -38,6 +38,27 @@ def load_and_process(path):
     # Make sure to return the latest dataframe
 
     return df2
+
+def filter_data_set(league, year):
+    
+    df= load_and_process("../../data/raw/understat.com.csv")
+    
+    df1=df[df['year']==year]
+    df2=df1[df1['league']==league]
+    df3=df2[df2['position']==1]
+
+    xG=[]
+
+    a= (float)(df3['xG'])
+    xG.append(a)
+    for i in range(0,19):
+        xG.append(a)
+    #print(xG)
+
+    df4=df2[df2['xG'].values >= xG]
+    #print(df2['xG'])
+    
+    return df4
 
 @dispatch(str,int)
 def data_filter(league,year):
